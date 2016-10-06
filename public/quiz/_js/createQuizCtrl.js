5a36a7ab0b91a872e465e52bfb59772783ed07d0
Brooks Becton <broabect@ut.utm.edu>
2016-10-06 15:23:26 -0500

    Improvments on create quiz form

--- public/quiz/_js/createQuizCtrl.js
+++ public/quiz/_js/createQuizCtrl.js
@@ -1,9 +1,46 @@
 angular.module("quizModule").controller('createQuizCtrl', function ($http, $mdSidenav, $route, $rootScope, $scope) {
 
-  $scope.newQuiz = {}
+  var _this = this;
+  $scope.multiChoiceLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
-  this.intToArray = function(num){
-    return new Array(num);   
+  this.addAnswer = function(questionId){
+    console.log("qId: " + questionId);
+    console.log(_this.createdQuiz);
+    _this.createdQuiz.questions[questionId].answers.push("");
   }
+
+  this.addQuestion = function(){
+    _this.createdQuiz.questions.push(_this.questionTemplate);
+  }
+
+  this.clearAnswer = function(questionId, answerId){
+    console.log("clearAnswer() " + answerId);
+    _this.createdQuiz.questions[questionId].answers.splice(answerId, 1);
+  }
+
+  this.createdQuiz = {
+    instructorName: "",
+    quizName: "",
+    public: false,
+    quizType: '',
+    questions: [
+      {
+        'text': "default",
+         'answers': [
+         "catfat"
+        ]
+      }
+    ]
+  }
+
+  this.intToArray = function (num) {
+    return new Array(num);
+  }
+
+  this.questionTemplate = {
+    'text': "",
+    'answers': [""]
+  }
+
 
 });
